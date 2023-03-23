740e16f2d482379b78abba7a381b76d884fb0be6
adithya <adithyapanchali20@gmail.com>
2023-03-23 22:01:21 +0530

    create BACKEND

---
+++ BACKEND/models/contract.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const hardwareSchema = new Schema({
+
+    hardwareID: {
+        type:String,
+        
+    },
+    hardwareName : {
+        type : String,
+        required : true,
+        unique: true,
+
+    },
+    address : {
+        type : String,
+        required : true
+    },
+    contact : {
+        type : Number,
+        required : true,
+        unique : true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique : true
+    },
+    // image : {
+    //     type : String,
+    //     required : true,
+    //     unique : true
+    // }
+    
+
+    
+})
+
+const Hardware = mongoose.model("Hardware",hardwareSchema);
+
+module.exports = Hardware;
---
+++ BACKEND/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+// app.use('/uploads', express.static('./uploads'));
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyparser.json());
+
+const URL = process.env.MONGODB_URL;
+
+
+mongoose.connect(URL, {
+    
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Mongodb connection success!");
+})
+
+const hardwareRouter = require("./routes/contracts.js");
+
+//app.use("/contracts",hardwareRouter);
+
+app.listen(PORT, () => {
+    console.log('Server is up and running on port number:' + PORT)
+})
