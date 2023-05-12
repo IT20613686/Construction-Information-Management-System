ccbdccd2ceb239be4c7ce5e7661ce3a61e36490e
IT20613686 <risedirisooriya1994@gmail.com>
2023-05-12 17:39:40 +0530

    Admin side dashboard created

---
+++ frontend/src/Components/Admin Dashboard/AdminDash.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminHeader from '../Admin Header/AdminHeader'
+import AdminCards from './AdminCards'
+
+function AdminDash() {
+  return (
+      <div>
+          <AdminHeader />
+          <AdminCards/>
+    </div>
+  )
+}
+
+export default AdminDash
